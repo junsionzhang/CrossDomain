e7f8c5311a35ddb95d3f27c8957eb411d2f978ef
junsionzhang <junsionzhang@gmail.com>
2017-06-13 10:29:06 +0800

    fix bug

--- webapp1/app.js
+++ webapp1/app.js
@@ -25,7 +25,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.listen(3000);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -44,5 +43,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.listen(3000);
 
 module.exports = app;
